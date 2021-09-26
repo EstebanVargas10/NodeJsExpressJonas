87d0429a00f9b4ebf27258c5d23beea75f4ceca0
Esteban <estebanvargas10@gmail.com>
2021-09-26 17:30:04 -0600

    Validation Added

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -5,19 +5,32 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint :)');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
