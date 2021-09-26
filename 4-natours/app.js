8ff3383f5f25db27ea9a8b2a0230a568ddabc5f8
Esteban <estebanvargas10@gmail.com>
2021-09-26 17:36:58 -0600

    Patch Added

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -61,6 +61,23 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    // if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No ID found',
+    });
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      tour: '<update Tour here>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
