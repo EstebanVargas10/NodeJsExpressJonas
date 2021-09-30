90284e7868f132d0fa42b9209f1ce3993a739dc1
Esteban <estebanvargas10@gmail.com>
2021-09-29 21:31:21 -0600

    Schema Added

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -18,9 +18,26 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    // console.log(con.connections);
     console.log('DB Connected');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
