54b8702c2ed1f6fd5502b47634b896a903903011
Esteban <estebanvargas10@gmail.com>
2021-10-02 17:46:37 -0600

    More columns on TourModals

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -6,14 +6,53 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Tour must have a name'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a Group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have a dificulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, //Remove white spaces
+    required: [true, 'Tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have a cover Image'],
+  },
+  images: {
+    type: [String],
+    createAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  startDates: [Date],
+  // endDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
