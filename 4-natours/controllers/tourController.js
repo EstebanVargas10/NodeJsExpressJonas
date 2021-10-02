c81abebcae8421c7cc2fd560ab8e55225dac6f4d
Esteban <estebanvargas10@gmail.com>
2021-10-01 21:59:33 -0600

    Models added

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,18 +1,5 @@
 const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No ID found',
-    });
-  }
-  next();
-};
+const Tour = require('./../dev-data/data/tours-simple.json');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -29,10 +16,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -40,32 +27,22 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
 
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tour,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   results: tours.length,
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'Sucess',
-        data: { tour: newTour },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'Sucess',
+    // data: { tour: newTour },
+  });
 };
 
 exports.updateTour = (req, res) => {
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -21,24 +21,6 @@ mongoose
     console.log('DB Connected');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
